c1b9c077f49ef189af1bf1f3ec85843b2b92696e
huang <1476615350@qq.com>
2024-03-17 20:10:25 +0800

    测试controller 1

---
+++ src/main/java/com/rms/backend/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.rms.backend.controller;
+
+/**
+ * @description: 测试
+ * @author: huang
+ * @date 2024年03月17日 20:07
+ **/
+
+public class TestController {
+
+
+    public void test1(){
+        System.out.println("test版本1");
+    }
+}
